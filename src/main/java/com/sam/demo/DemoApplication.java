eacb4347e31fef54a11dfeeca7402e0d1ab25e0a
sampath <sampathn7890@gmail.com>
2022-05-14 00:55:16 -0700

    dockerfile

--- src/main/java/com/sam/demo/DemoApplication.java
+++ src/main/java/com/sam/demo/DemoApplication.java
@@ -2,12 +2,27 @@ package com.sam.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	@GetMapping("/")
+    public String hello() {
+        
+        return "hello";
+    }
+	
+	@GetMapping("/sampath")
+    public String helloSampath() {
+        
+        return "hello Sampath";
+    }
 
 }
